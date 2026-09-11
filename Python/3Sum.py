dcf70491b1cfcbe91300513c02e1d98a59c62989
Agilesh P <144360612+Agilesh03@users.noreply.github.com>
2026-09-11 18:39:09 +0530

    Solve: 3Sum solution

---
+++ Python/3Sum.py
@@ -0,0 +1,20 @@
+                    left += 1
+
+                elif total > 0:
+                    right -= 1
+
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicate left values
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    # Skip duplicate right values
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return result
